2ca626e3515152fa0836c675468b38aee57fed96
sofia-kindratyshyn <sofia25kind@gmail.com>
2025-09-01 13:57:47 +0300

    Added suspense

--- frontend/src/app/(private routes)/trains/actions/update/page.tsx
+++ frontend/src/app/(private routes)/trains/actions/update/page.tsx
@@ -2,10 +2,17 @@
 
 import { useSearchParams } from "next/navigation";
 import UpdateTrainForm from "../../../../../../components/UpdateTrainForm";
+import { Suspense } from "react";
 
 export default function TrainUpdatePage() {
   const searchParams = useSearchParams();
   const trainId = searchParams.get("trainId");
 
-  return <>{trainId && <UpdateTrainForm trainId={Number(trainId)} />}</>;
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        {trainId && <UpdateTrainForm trainId={Number(trainId)} />}
+      </Suspense>
+    </>
+  );
 }
