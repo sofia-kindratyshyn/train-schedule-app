4713b2bcec0bf3b8da2b668b9507157eb8d44d20
Sofia Kindratyshyn <sofia25kind@gmail.com>
2025-09-01 16:11:46 +0300

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,28 +1,28 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+//import { NextResponse } from 'next/server';
+//import type { NextRequest } from 'next/server';
 
-const privateRoutes = ['/profile', '/notes', '/trains'];
-const publicRoutes = ['/sign-in', '/sign-up'];
+//const privateRoutes = ['/profile', '/notes', '/trains'];
+//const publicRoutes = ['/sign-in', '/sign-up'];
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+//export function middleware(request: NextRequest) {
+  //const { pathname } = request.nextUrl;
 
-  const accessToken = request.cookies.get('accessToken')?.value;
-  const refreshToken = request.cookies.get('refreshToken')?.value;
+ // const accessToken = request.cookies.get('accessToken')?.value;
+ // const refreshToken = request.cookies.get('refreshToken')?.value;
 
-  const isPrivateRoute = privateRoutes.some(route => pathname.startsWith(route));
-  const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route));
+  //const isPrivateRoute = privateRoutes.some(route => pathname.startsWith(route));
+  //const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route));
 
-  if (isPrivateRoute && !accessToken) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
-  }
+ // if (isPrivateRoute && !accessToken) {
+   // return NextResponse.redirect(new URL('/sign-in', request.url));
+ // }
 
-  if (isPublicRoute && accessToken) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+ // if (isPublicRoute && accessToken) {
+ //   return NextResponse.redirect(new URL('/', request.url));
+//  }
 
-  return NextResponse.next();
-}
+//  return NextResponse.next();
+//}
 
 export const config = {
   matcher: ['/profile/:path*', '/notes/:path*', '/trains/:path*', '/sign-in', '/sign-up'],
