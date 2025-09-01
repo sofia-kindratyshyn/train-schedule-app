219dd60895280bebee2682fd9c86aa294d40a2e5
sofia-kindratyshyn <sofia25kind@gmail.com>
2025-09-01 14:12:36 +0300

    added prefetching for trains

--- frontend/src/app/(private routes)/trains/page.tsx
+++ frontend/src/app/(private routes)/trains/page.tsx
@@ -1,11 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import TrainsList from "../../../../components/TrainsList";
 import { getTrains } from "../../../../lib/api/clientApi";
+import { Train } from "../../../../types/train";
 
-export default async function TrainsPage() {
-  const trainsList = await getTrains();
-  return (
-    <>
-      <TrainsList trains={trainsList} />
-    </>
-  );
+export default function TrainsPage() {
+  const [trains, setTrains] = useState<Train[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchTrains() {
+      try {
+        const trainsData = await getTrains();
+        setTrains(trainsData);
+      } catch (error) {
+        console.error("Error fetching trains:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchTrains();
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50vh",
+        }}
+      >
+        <div>Loading trains...</div>
+      </div>
+    );
+  }
+
+  return <TrainsList trains={trains} />;
 }
