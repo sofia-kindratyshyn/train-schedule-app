0a98947f2f71fdccda5ebe6615fe555ebaf4aa83
Sofia Kindratyshyn <sofia25kind@gmail.com>
2025-09-02 02:32:35 +0300

    Update api.ts

--- frontend/lib/api/api.ts
+++ frontend/lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const nextServer = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'process.env.NEXT_PUBLIC_API_URL',
     withCredentials: true,
 })
