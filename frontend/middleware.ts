3c00de6e7266ff7a77d606b944719ea2f121a6bf
sofia-kindratyshyn <sofia25kind@gmail.com>
2025-09-02 04:17:42 +0300

    Edited middlewar

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value
 
   if ((pathname.startsWith('/trains') || pathname.startsWith('/profile')) && !accessToken) {
-    return NextResponse.redirect(new URL('/sign-in', request.url))
+    return NextResponse.redirect(new URL(`/sign-in?redirect=${pathname}`, request.url))
   }
 
   if ((pathname.startsWith('/sign-in') || pathname.startsWith('/sign-up')) && accessToken) {
